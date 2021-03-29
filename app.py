e8b74432f8956858d6e93a6bde3e0e98a02a105d
dimitar-papalazov <dimitar.papalazov@students.finki.ukim.mk>
2021-03-29 17:07:46 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import connexion
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+connexion_app = connexion.App(__name__, specification_dir="./")
+app = connexion_app.app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+connexion_app.add_api("api.yml")
+
+# endpoints
+# TODO
+
+# dummy reference for migrations only
+# TODO
+
+if __name__ == "__main__":
+    connexion_app.run(host='0.0.0.0', port=5000, debug=True)
